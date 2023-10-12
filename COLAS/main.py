2b6caff010c6aea15826bd774257ec0285df2581
FlavioMer <131312488+FlavioMer@users.noreply.github.com>
2023-10-12 07:36:22 -0600

    modified

--- COLAS/main.py
+++ COLAS/main.py
@@ -7,30 +7,30 @@ class Nodo:
 class Cola:
 
     def __init__(self):
-        self.frente = None
+        self.inicio = None
         self.final = None
 
     def encolar(self, dato):
         print(f"Agregando {dato} al final de la cola")
         nuevo_nodo = Nodo(dato)
         if self.final is None:
-            self.frente = self.final = nuevo_nodo
+            self.inicio = self.final = nuevo_nodo
             return
         self.final.siguiente = nuevo_nodo
         self.final = nuevo_nodo
 
     def desencolar(self):
-        print(f"Desencolando {self.frente.dato}")
-        self.frente = self.frente.siguiente
-        if self.frente is None:
+        print(f"Desencolando {self.inicio.dato}")
+        self.inicio = self.inicio.siguiente
+        if self.inicio is None:
             self.final = None
 
     def listar(self):
-        if self.frente is None:
+        if self.inicio is None:
             print("La cola está vacía")
             return
         print("Imprimiendo cola:")
-        nodo_aux = self.frente
+        nodo_aux = self.inicio
         while nodo_aux:
             print(f"{nodo_aux.dato}", end=", ")
             nodo_aux = nodo_aux.siguiente
